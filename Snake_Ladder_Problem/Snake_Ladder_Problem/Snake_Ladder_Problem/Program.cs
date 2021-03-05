3fbf1a557f90e3314d4c4ef475b0e5ddfbe0467c
Kajal <kajalpatil158@gmail.com>
2021-03-05 21:24:27 +0530

    [Kajal]ADD: Roll_Die

--- Snake_Ladder_Problem/Snake_Ladder_Problem/Snake_Ladder_Problem/Program.cs
+++ Snake_Ladder_Problem/Snake_Ladder_Problem/Snake_Ladder_Problem/Program.cs
@@ -8,7 +8,11 @@ namespace Snake_Ladder_Problem
         {
                 int Player1 = 0;
                 Console.WriteLine("WELL COME TO SNAKE LADDER GAME.Player1 Start Game With: "+Player1);
-               
+                Random random = new Random();
+                int Die = random.Next(0, 7);
+                {
+                    Console.WriteLine("Player1 Get A Number:" + Die);
+                }
         }
     }
 }
