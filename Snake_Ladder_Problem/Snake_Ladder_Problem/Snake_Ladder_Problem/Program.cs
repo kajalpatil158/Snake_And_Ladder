8d85f06039b3d0edc0db55e98d96e612b1f0fbfd
Kajal <kajalpatil158@gmail.com>
2021-03-05 21:16:54 +0530

    [Kajal]ADD: Snake And Ladder Game Start With 0 Position

---
+++ Snake_Ladder_Problem/Snake_Ladder_Problem/Snake_Ladder_Problem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Snake_Ladder_Problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                int Player1 = 0;
+                Console.WriteLine("WELL COME TO SNAKE LADDER GAME.Player1 Start Game With: "+Player1);
+               
+        }
+    }
+}
